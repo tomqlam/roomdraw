0eb3161c99aa66f85a1f16d36b66a4e030d77910
Tom Lam <tomqlam@gmail.com>
2024-02-23 21:19:15 -0800

    fook

--- frontend2/src/MyContext.js
+++ frontend2/src/MyContext.js
@@ -79,7 +79,7 @@ export const MyContextProvider = ({ children }) => {
 
 
     function fetchUserMap() {
-        fetch(`https://www.cs.hmc.edu/~tlam/roomdraw/users/idmap`)
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/users/idmap`)
             .then(res => {
                 return res.json();
             })
