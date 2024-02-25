f168d7e5448a0d3aecc33f059351e89e6c920e2f
Tom Lam <tomqlam@gmail.com>
2024-02-24 17:17:36 -0800

    patch -> put

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -51,14 +51,14 @@ func main() {
 	router.GET("/rooms", middleware.JWTAuthMiddleware(), handlers.GetRoomsHandler) // Read
 	router.GET("/rooms/simple/:dormName", handlers.GetSimpleFormattedDorm)         // Read
 	router.GET("/rooms/simpler/:dormName", handlers.GetSimplerFormattedDorm)       // Read
-	router.PATCH("/rooms/:roomuuid", handlers.UpdateRoomOccupants)                 // Write
+	router.PUT("/rooms/:roomuuid", handlers.UpdateRoomOccupants)                   // Write
 	router.GET("/users", handlers.GetUsers)                                        // Read
 	router.GET("/users/idmap", middleware.JWTAuthMiddleware(), handlers.GetUsersIdMap)
 	router.POST("/suites/design", handlers.SetSuiteDesign)
 
 	router.POST("/frosh/:roomuuid", handlers.AddFroshHandler)
 	router.DELETE("/frosh/:roomuuid", handlers.RemoveFroshHandler)
-	router.PATCH("/frosh/:roomuuid", handlers.BumpFroshHandler)
+	router.PUT("/frosh/:roomuuid", handlers.BumpFroshHandler)
 
 	// Start the server
 	router.Run(config.ServerAddress)
