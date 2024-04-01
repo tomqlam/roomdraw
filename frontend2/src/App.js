31d6bd35ea6707ecad2c16c531687e918b1b664c
Serena Mao <seranamao2003@gmail.com>
2024-03-31 20:56:50 -0700

    Add notification for UI update

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -50,7 +50,19 @@ function App() {
 
   } = useContext(MyContext);
 
-  // const [showNotification, setShowNotification] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const hasClosedNotification = localStorage.getItem('hasClosedNotification');
+    setShowNotification(hasClosedNotification !== 'true');
+  }, []);
+
+  const handleCloseNotification = () => {
+    // Store the notification state in local storage when the user closes the notification
+    localStorage.setItem('hasClosedNotification', 'true');
+    setShowNotification(false);
+  };
+
   const [myRoom, setMyRoom] = useState("You are not in a room yet."); // to show what room current user is in
   const [isBurgerClicked, setIsBurgerClicked] = useState(false);
   const [isInDorm, setIsInDorm] = useState(true);
@@ -357,6 +369,11 @@ function App() {
       {isSuiteNoteModalOpen && <SuiteNoteModal />}
       {isFroshModalOpen && <BumpFroshModal />}
       {isSettingsModalOpen && <SettingsModal />}
+
+      {(credentials && showNotification) && <div class="notification is-link" style={{ marginLeft: '20px', marginRight: '20px' }}>
+        <button class="delete" onClick={handleCloseNotification}></button>
+        UI Update: 1) Preplaced rooms now appear slightly darker by default. 2) Unbumpable rooms are now always black.
+      </div>}
 
 
 
